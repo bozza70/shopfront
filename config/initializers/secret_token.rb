4ddc73f76d98bd426c549a5f522a2e5062be38bf
Bozza70 <bozza70@gmail.com>
2012-11-06 07:11:41 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopfront::Application.config.secret_token = 'b9c001a355cfeed719ff85c8bc0a5cc8d29e584508a26b858da8d7b4a700854f66a3e1c0e519978a7be7031f959cfea8e3f11543dc3ff228fdd98e3481164767'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Shopfront::Application.config.session_store :cookie_store, key: '_shopfront_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Shopfront::Application.config.session_store :active_record_store
